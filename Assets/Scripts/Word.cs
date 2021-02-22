a686b4468dfdf2f9b617ccaec59ef2a7b08d25c4
3174N <eitan.levy.159@gmail.com>
2021-02-22 15:08:27 +0200

    Add word class

---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] 
+public class Word
+{
+    public enum wordType
+    {
+        Noun,
+        Verb,
+        Adjactive
+    }
+
+    public enum wordPurpose
+    {
+        Nose,
+        Nasu,
+        MashlimShem,
+        MashlimPoal
+    }
+
+    #region Variables
+    public wordType Type;
+
+    public wordPurpose Purpose; 
+
+    #endregion
+}
