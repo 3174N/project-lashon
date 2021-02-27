f341bc9cd3d8ab253b514571f1218b7a854e1940
3174N <3174N@protonmail.com>
2021-02-27 21:00:14 +0200

    Create LevelLoader.cs

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader
+{
+    /// <summary>
+    /// Reloads current scene.
+    /// </summary>
+    public static void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Loads a specific scene
+    /// </summary>
+    /// <param name="index">Scene's build index</param>
+    public static void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    /// <summary>
+    /// Loads a specific scene
+    /// </summary>
+    /// <param name="index">Scene's build index</param>
+    public static void LoadLevel(string index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
